216712ad2f7cda38a2e5c0b7c3d6b97320a01e7f
Denis Roussel <denis.roussel@gmail.com>
2012-07-31 17:14:26 +0200

    ajout d'une fonction underscore

--- src/Cungfoo/Lib/Utils.php
+++ src/Cungfoo/Lib/Utils.php
@@ -12,5 +12,11 @@ class Utils
     public static function camelize($word)
     {
         return lcfirst(str_replace(" ", "", ucwords(strtr($word, "_-", "  "))));
+        //return preg_replace_callback('/(^|_|\.)+(.)/', function ($match) { return ('.' === $match[1] ? '_' : '').strtoupper($match[2]); }, $string);
+    }
+
+    public function underscore($string)
+    {
+        return strtolower(preg_replace(array('/([A-Z]+)([A-Z][a-z])/', '/([a-z\d])([A-Z])/'), array('\\1_\\2', '\\1_\\2'), strtr($string, '_', '.')));
     }
 }
