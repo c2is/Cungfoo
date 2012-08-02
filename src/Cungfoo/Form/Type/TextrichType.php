6d103fded032eb0ca15ab505d8fc8f7438b6f68b
Morgan Brunot <brunot.morgan@gmail.com>
2012-08-02 12:24:31 +0200

    refacto du custom widget textrich

--- src/Cungfoo/Form/Type/TextrichType.php
+++ src/Cungfoo/Form/Type/TextrichType.php
@@ -1,13 +1,5 @@
 <?php
 
-/**
- * This file is part of the PropelBundle package.
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * @license    MIT License
- */
-
 namespace Cungfoo\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
@@ -15,13 +7,10 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-use Cungfoo\Form\ChoiceList\ModelChoiceList;
-use Cungfoo\Form\DataTransformer\CollectionToArrayTransformer;
-
 /**
- * ModelType class.
+ * TextrichType class.
  *
- * @author William Durand <william.durand1@gmail.com>
+ * @author Morgan Brunto <brunot.morgan@gmail.com>
  */
 class TextrichType extends AbstractType
 {
