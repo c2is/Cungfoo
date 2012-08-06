163c5e8b60a24097db86386a9a13d903a969c761
Denis Roussel <denis.roussel@gmail.com>
2012-08-06 16:07:39 +0200

    refacto du CRUD controller

--- src/Cungfoo/Controller/CrudController.php
+++ src/Cungfoo/Controller/CrudController.php
@@ -2,29 +2,32 @@
 
 namespace Cungfoo\Controller;
 
-use Silex\Application;
-use Silex\ControllerCollection;
-use Silex\ControllerProviderInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Routing\Route;
+use Silex\Application,
+    Silex\ControllerCollection,
+    Silex\ControllerProviderInterface;
+
+use Symfony\Component\HttpFoundation\Request,
+    Symfony\Component\HttpKernel\Exception\NotFoundHttpException,
+    Symfony\Component\Routing\Route;
 
 /**
- * @author Morgan Brunot <brunot.morgan@gmail.com>
+ * CRUD controllers provider.
+ *
+ * @author  Morgan Brunot <brunot.morgan@gmail.com>
+ * @author  Denis Roussel <denis.roussel@gmail.com>
  */
 class CrudController implements ControllerProviderInterface
 {
-    private $modelName;
-
-    private $modelClass;
-
-    private $formType;
+    protected $modelName;
+    protected $modelClass;
+    protected $formType;
 
     public function __construct($modelName, $modelClass, $formType)
     {
         $this->modelName  = $modelName;
         $this->modelClass = $modelClass;
+        $this->queryClass = $modelClass.'Query';
+        $this->peerClass  = $modelClass.'Peer';
         $this->formType   = $formType;
     }
 
@@ -33,183 +36,139 @@ class CrudController implements ControllerProviderInterface
      */
     public function connect(Application $app)
     {
-        $modelName  = $this->modelName;
-        $modelClass = $this->modelClass;
-        $formType   = $this->formType;
-
-        $prefix     = sprintf('crud_controller.%s.', $this->modelName);
-
-        if (null !== $this->modelClass)
+        if (!class_exists($this->modelClass))
         {
-            $app[$prefix.'model_class'] = $this->modelClass;
-        }
-
-        if (isset($app[$prefix.'model_class']))
-        {
-            $app[$prefix.'query_class'] = $app[$prefix.'model_class'] . 'Query';
-        } else
-        {
-            throw new \InvalidArgumentException(
-                sprintf('You have to configure the "%s.model_class" parameter.', $prefix)
-            );
+            throw new \Exception(sprintf('Class %s undefined.', $this->modelClass));
         }
 
         $controllers = $app['controllers_factory'];
 
-        // Returns all objects
-        $controllers->get('/', function () use ($app, $prefix)
-        {
-            $query = new $app[$prefix.'query_class'];
-
-            $peerName = sprintf('%sPeer', $this->modelClass);
-
-            return $app['twig']->render('Crud/list.twig', array(
-                'name'       => $this->modelName,
-                'fieldnames' => $peerName::getFieldNames(),
-                'collection' => $query->find()->exportTo($app['twig_collection_parser']),
-            ));
-        })
-        ->bind(sprintf('%s_crud_list', $modelName))
-        ;
-
-        // Returns a specific object identified by a given id
-        $controllers->get('/{id}/show', function ($id) use ($app, $prefix)
-        {
-            $query  = new $app[$prefix.'query_class'];
-            $object = $query->findPk($id);
-
-            if (!$object instanceof $app[$prefix.'model_class'])
-            {
-                throw new NotFoundHttpException(
-                    sprintf('%s with id "%d" does not exist.', ucfirst($this->modelName), $id)
-                );
-            }
-
-            $peerName = sprintf('%sPeer', $this->modelClass);
-
-            return $app['twig']->render('Crud/show.twig', array(
-                'name'       => $this->modelName,
-                'fieldnames' => $peerName::getFieldNames(),
-                'object'     => $object->exportTo($app['twig_object_parser']),
-            ));
-        })
-        ->bind(sprintf('%s_crud_show', $modelName))
-        ;
-
-        //Create a new object
-        $controllers->get('/edit/{id}', function ($id = null, Request $request) use ($app, $prefix)
-        {
-            $peerName  = sprintf('%sPeer', $this->modelClass);
-            $queryName = sprintf('%sQuery', $this->modelClass);
-
-            if (null == $id)
-            {
-                $model = new $this->modelClass();
-            }
-            else
-            {
-                $model = $queryName::create()
-                    ->filterById($id)
-                    ->findOne()
-                ;
-
-                if (null == $model)
-                {
-                    throw new \Exception('Object not found');
-                }
-            }
-
-            if (!class_exists($this->formType))
-            {
-                throw new \Exception('Définir la classe');
-            }
-
-            $form = $app['form.factory']->create(new $this->formType($app), $model);
-
-            // display the form
-            return $app['twig']->render('Crud/edit.twig', array(
-                'name' => $this->modelName,
-                'form' => $form->createView(),
-            ));
-
-        })
-        ->value('id', null)
-        ->bind(sprintf('%s_crud_edit', $modelName))
-        ;
-
-        // Create a new object
-        $controllers->post('/edit/{id}', function ($id = null, Request $request) use ($app, $prefix)
-        {
-            $peerName  = sprintf('%sPeer', $this->modelClass);
-            $queryName = sprintf('%sQuery', $this->modelClass);
-
-            if (null == $id)
-            {
-                $model = new $this->modelClass();
-            }
-            else
-            {
-                $model = $queryName::create()
-                    ->filterById($id)
-                    ->findOne()
-                ;
-
-                if (null == $model)
-                {
-                    throw new \Exception('Object not found');
-                }
-            }
-
-            if (!class_exists($this->formType))
-            {
-                throw new \Exception('Définir la classe');
-            }
-
-            $form = $app['form.factory']->create(new $this->formType($app), $model);
-
-            if ('POST' == $request->getMethod())
-            {
-                $form->bind($app['request']->get($form->getName()));
-
-                if ($form->isValid())
-                {
-                    $model->save();
-
-                    return $app->redirect($app['url_generator']->generate(sprintf('%s_crud_show', $this->modelName), array('id' => $model->getId())));
-                }
-            }
-
-            // display the form
-            return $app['twig']->render('Crud/edit.twig', array(
-                'name' => $this->modelName,
-                'form' => $form->createView()
-            ));
-        })
-        ->value('id', null)
-        ->bind(sprintf('%s_crud_create', $modelName))
-        ;
-
-        // Delete a object identified by a given id
-        $controllers->get('/delete/{id}', function ($id) use ($app, $prefix)
-        {
-            $queryName = sprintf('%sQuery', $this->modelClass);
-            $model = $queryName::create()
-                ->filterById($id)
-                ->findOne()
-            ;
-
-            if (null == $model)
-            {
-                throw new \Exception('Object not found');
-            }
-
-            $model->delete();
-
-            return $app->redirect($app['url_generator']->generate(sprintf('%s_crud_list', $this->modelName)));
-        })
-        ->bind(sprintf('%s_crud_delete', $modelName))
+        $this
+            ->generateList    ($app, $controllers)
+            ->generateRead    ($app, $controllers)
+            ->generateUpdate  ($app, $controllers)
+            ->generateDelete  ($app, $controllers)
         ;
 
         return $controllers;
+    }
+
+    protected function generateList(Application $app, ControllerCollection $controllers)
+    {
+        $controllers
+            ->get('/', function () use ($app)
+            {
+                $query = new $this->queryClass();
+
+                return $app['twig']->render('Crud/list.twig', array(
+                    'name'       => $this->modelName,
+                    'fieldnames' => call_user_func($this->peerClass.'::getFieldNames'),
+                    'collection' => $query->find()->exportTo($app['twig_collection_parser']),
+                ));
+            })
+            ->bind(sprintf('%s_crud_list', $this->modelName))
+        ;
+        return $this;
+    }
+
+    protected function generateRead(Application $app, ControllerCollection $controllers)
+    {
+        $controllers
+            ->get('/{id}', function ($id) use ($app)
+            {
+                $query  = new $this->queryClass();
+                $object = $query->findPk($id);
+
+                if ($object === null)
+                {
+                    throw new NotFoundHttpException(sprintf('%s with id "%d" does not exist.', ucfirst($this->modelName), $id));
+                }
+
+                return $app['twig']->render('Crud/read.twig', array(
+                    'name'       => $this->modelName,
+                    'fieldnames' => call_user_func($this->peerClass.'::getFieldNames'),
+                    'object'     => $object->exportTo($app['twig_object_parser']),
+                ));
+            })
+            ->bind(sprintf('%s_crud_read', $this->modelName))
+        ;
+        return $this;
+    }
+
+    protected function generateUpdate(Application $app, ControllerCollection $controllers)
+    {
+        $controllers
+            ->match('/{id}/update', function ($id = null, Request $request) use ($app)
+            {
+                if ($id === null)
+                {
+                    $object = new $this->modelClass();
+                }
+                else
+                {
+                    $object = call_user_func($this->queryClass.'::create')
+                        ->filterById($id)
+                        ->findOne()
+                    ;
+
+                    if ($object === null)
+                    {
+                        throw new NotFoundHttpException(sprintf('%s with id "%d" does not exist.', ucfirst($this->modelName), $id));
+                    }
+                }
+
+                if (!class_exists($this->formType))
+                {
+                    throw new \Exception(sprintf('Class %s undefined.', $this->formType));
+                }
+
+                $form = $app['form.factory']->create(new $this->formType($app), $object);
+
+                if ('POST' == $request->getMethod())
+                {
+                    $form->bind($app['request']->get($form->getName()));
+
+                    if ($form->isValid())
+                    {
+                        $object->save();
+                        return $app->redirect($app['url_generator']->generate(sprintf('%s_crud_read', $this->modelName), array('id' => $object->getId())));
+                    }
+                }
+
+                // display the form
+                return $app['twig']->render('Crud/update.twig', array(
+                    'name' => $this->modelName,
+                    'form' => $form->createView(),
+                ));
+
+            })
+            ->value('id', null)
+            ->bind(sprintf('%s_crud_update', $this->modelName))
+        ;
+        return $this;
+    }
+
+    protected function generateDelete(Application $app, ControllerCollection $controllers)
+    {
+        $controllers
+            ->get('/delete/{id}', function ($id) use ($app)
+            {
+                $object = call_user_func($this->queryClass.'::create')
+                    ->filterById($id)
+                    ->findOne()
+                ;
+
+                if ($object === null)
+                {
+                    throw new NotFoundHttpException(sprintf('%s with id "%d" does not exist.', ucfirst($this->modelName), $id));
+                }
+
+                $object->delete();
+
+                return $app->redirect($app['url_generator']->generate(sprintf('%s_crud_list', $this->modelName)));
+            })
+            ->bind(sprintf('%s_crud_delete', $this->modelName))
+        ;
+        return $this;
     }
 }
