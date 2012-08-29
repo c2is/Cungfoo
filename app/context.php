941696720d6cf97a725c9e72ab1bdef72e2973c4
Morgan Brunot <brunot.morgan@gmail.com>
2012-08-29 10:33:25 +0200

    passer cungfoo en français par défaut

--- app/context.php
+++ app/context.php
@@ -7,3 +7,6 @@ $app['context'] = $app->share(function() {
 $app['context']->addParam('language', isset($_GET['Context']['language']) ? $_GET['Context']['language'] : 0);
 $app['context']->addParam('site', isset($_GET['Context']['site']) ? $_GET['Context']['site'] : 0);
 $app['context']->addParam('saison', isset($_GET['Context']['saison']) ? $_GET['Context']['saison'] : 0);
+
+// set fr default
+$app['translator']->setLocale('fr');
