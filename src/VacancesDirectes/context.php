6ef89bc8306d11632201b29cf6cb0e405edce2c3
Morgan Brunot <brunot.morgan@gmail.com>
2012-09-04 14:36:33 +0200

    Sélection du bon logo selon la langue (définit par le domaine)

--- src/VacancesDirectes/context.php
+++ src/VacancesDirectes/context.php
@@ -1,8 +1,16 @@
 <?php
-
-$app['context']->addParam('language', isset($_GET['locale']) ? $_GET['locale'] : 'fr');
+/*
+ * Configuration of our context application
+ *
+ * @author  Morgan Brunot <brunot.morgan@gmail.com>
+ *          Denis Roussel <denis.roussel@gmail.com>
+ *
+ * @package Cungfoo by C2IS
+ */
 $app['context']->addParam('site', isset($_GET['site']) ? $_GET['site'] : 'indiv');
 $app['context']->addParam('domaine', isset($_GET['domaine']) ? $_GET['domaine'] : 'fr');
+
+$app['context']->addParam('language', isset($_GET['domaine']) ? $_GET['domaine'] : $app['context']->get('domaine'));
 
 // set fr default
 $app['translator']->setLocale($app['context']->get('language'));
