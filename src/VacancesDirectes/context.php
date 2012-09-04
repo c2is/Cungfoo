b6c4f60819cdb1eab2124200092d803f758cdca1
Morgan Brunot <brunot.morgan@gmail.com>
2012-09-04 12:22:14 +0200

    Règle de mise à jour du context GET locale pour définir l'extension du domaine (defaut: fr) GET site pour définir le sous-domaine (defaut: indiv)

--- src/VacancesDirectes/context.php
+++ src/VacancesDirectes/context.php
@@ -2,7 +2,7 @@
 
 $app['context']->addParam('language', isset($_GET['locale']) ? $_GET['locale'] : 'fr');
 $app['context']->addParam('site', isset($_GET['site']) ? $_GET['site'] : 'indiv');
-$app['context']->addParam('saison', isset($_GET['domaine']) ? $_GET['domaine'] : 'fr');
+$app['context']->addParam('domaine', isset($_GET['domaine']) ? $_GET['domaine'] : 'fr');
 
 // set fr default
 $app['translator']->setLocale($app['context']->get('language'));
