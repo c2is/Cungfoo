b7e1bf14955e7bb23db57c60bbde0f203b285a94
Morgan Brunot <brunot.morgan@gmail.com>
2012-09-04 15:52:10 +0200

    liste déroulante de sélection de langue dans le front

--- src/VacancesDirectes/context.php
+++ src/VacancesDirectes/context.php
@@ -7,9 +7,8 @@
  *
  * @package Cungfoo by C2IS
  */
-$app['context']->addParam('site', isset($_GET['site']) ? $_GET['site'] : 'indiv');
+$app['context']->addParam('site', isset($_GET['site']) ? $_GET['site'] : 'site.individual');
 $app['context']->addParam('domaine', isset($_GET['domaine']) ? $_GET['domaine'] : 'fr');
-
 $app['context']->addParam('language', isset($_GET['domaine']) ? $_GET['domaine'] : $app['context']->get('domaine'));
 
 // set fr default
