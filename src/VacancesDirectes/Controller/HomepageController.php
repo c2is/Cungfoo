f15efa1990c40f49d3a9aae7f455280fe13d7ae7
Denis Roussel <denis.roussel@gmail.com>
2012-09-13 09:45:26 +0200

    correction du controleur principal

--- src/VacancesDirectes/Controller/HomepageController.php
+++ src/VacancesDirectes/Controller/HomepageController.php
@@ -26,21 +26,6 @@ class HomepageController implements ControllerProviderInterface
 
         $controllers->match('/', function (Request $request) use ($app)
         {
-            var_dump($app['memcache']->get('data1', function() use($app) { 
-                return "toto";
-            }));
-            var_dump($app['memcache']->get('data1'));
-            var_dump($app['memcache']->set('data1', 256));
-            var_dump($app['memcache']->get('data1'));
-            //$app['memcache']->delete('data1');
-            var_dump($app['memcache']->get('data1'));
-
-            // $app['memcache']->set('data1', array('some' => 'data')));
-            // $app['memcache']->delete('some.data');
-
-
-            die('in');
-
             $locale = $app['context']->get('language');
 
             /** @var \Cungfoo\Model\Etablissement $etablissement  */
