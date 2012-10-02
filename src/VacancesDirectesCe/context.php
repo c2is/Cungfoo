74e2b56737f98d5f063675c2a98a67444b2a6abd
Morgan Brunot <brunot.morgan@gmail.com>
2012-10-02 17:30:46 +0200

    initialisation du site CE * création d'un nouveau bundle VacancesDirectesCe * création d'un controller homepage vide

---
+++ src/VacancesDirectesCe/context.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Configuration of our context application
+ *
+ * @author  Morgan Brunot <brunot.morgan@gmail.com>
+ *          Denis Roussel <denis.roussel@gmail.com>
+ *
+ * @package Cungfoo by C2IS
+ */
+$app['context']->addParam('site', isset($_GET['site']) ? $_GET['site'] : 'site.ce');
+$app['context']->addParam('domaine', isset($_GET['domaine']) ? $_GET['domaine'] : 'fr');
+$app['context']->addParam('language', isset($_GET['domaine']) ? $_GET['domaine'] : $app['context']->get('domaine'));
+
+// check if domain is allow for this site
+if (!in_array($app['context']->get('domaine'), $app['config']->get('dimensions')['site'][$app['context']->get('site')]['domaines']))
+{
+    throw new \Exception(sprintf("`%s` domain is not available for `%s` site.", $app['context']->get('domaine'), $app['context']->get('site')));
+}
+
+// set current language
+$app['translator']->setLocale($app['context']->get('language'));
---
+++ web/ce_dev.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Development application builder
+ *
+ * @author  Morgan Brunot <brunot.morgan@gmail.com>
+ *          Denis Roussel <denis.roussel@gmail.com>
+ *
+ * @package Cungfoo by C2IS
+ */
+
+use Symfony\Component\ClassLoader\DebugClassLoader;
+use Symfony\Component\HttpKernel\Debug\ErrorHandler;
+use Symfony\Component\HttpKernel\Debug\ExceptionHandler;
+
+ini_set('display_errors', 0);
+
+// loaded the libraries
+require_once __DIR__.'/../vendor/autoload.php';
+
+ini_set('display_errors', 1);
+error_reporting(-1);
+DebugClassLoader::enable();
+ErrorHandler::register();
+if ('cli' !== php_sapi_name())
+{
+    ExceptionHandler::register();
+}
+
+// created the application
+$app = require __DIR__ . '/../src/VacancesDirectesCe/app.php';
+
+// created the context
+require __DIR__ . '/../src/VacancesDirectesCe/context.php';
+
+// load routes code
+require __DIR__ . '/../src/VacancesDirectesCe/routes.php';
+
+// set environnement
+require __DIR__ . '/../app/config/dev.php';
+
+$app->run();
