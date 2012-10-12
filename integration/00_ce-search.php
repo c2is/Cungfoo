a02ac782d8ff078426136d197096e0c54d28ad53
Nicolas CHAMALET <n.chamalet@c2is.fr>
2012-10-12 15:03:21 +0200

    [RECHERCHE] integration select multiple

--- integration/00_ce-search.php
+++ integration/00_ce-search.php
@@ -4,7 +4,7 @@ $page = 'ce';
 include('includes/inc_header.php');
 include('includes/ce-top.php'); ?>
 
-<div id="wrap" class="fixed-width clear">
+<div id="wrap" class="fixed-width clear" xmlns="http://www.w3.org/1999/html">
 
     <!-- colonne pleine largeur -->
     <div id="headerContainer" class="column">
@@ -45,12 +45,12 @@ include('includes/ce-top.php'); ?>
     <div id="searchContainer" class="column clearboth">
         <div id="searchPrincipal" class="searchBox classic">
 
-            <form id=searchForm>
+            <form id="searchForm">
                 <fieldset>
                     <legend>Recherche de linéaires classiques</legend>
                     <ol>
                         <li>
-                            <div class="selectContainer clear"></div>
+                            <div class="selectContainer clear">
                                 <select name="selectCountry" onchange="">
                                     <option value="1">Country 1</option>
                                     <option value="2">Country 2</option>
@@ -64,35 +64,33 @@ include('includes/ce-top.php'); ?>
                             </div>
                         </li>
                         <li>
-                            <p>
-                                <input id="camping1" name="camping1" value="1" type="checkbox"><label for="camping1">Camping 1</label>
-                            </p>
-                            <p>
-                                <input id="camping2" name="camping2" value="2" type="checkbox"><label for="camping2">Camping 2</label>
-                            </p>
-                            <p>
-                                <input id="camping3" name="camping3" value="3" type="checkbox"><label for="camping3">Camping 3</label>
-                            </p>
-                            <p>
-                                <input id="camping4" name="camping4" value="4" type="checkbox"><label for="camping4">Camping 4</label>
-                            </p>
-                            <p>
-                                <input id="camping5" name="camping5" value="5" type="checkbox"><label for="camping5">Camping 5</label>
-                            </p>
-                            <p>
-                                <input id="camping6" name="camping6" value="6" type="checkbox"><label for="camping6">Camping 6</label>
-                            </p>
-                            <p>
-                                <input id="camping7" name="camping7" value="7" type="checkbox"><label for="camping7">Camping 7</label>
-                            </p>
-                            <p>
-                                <input id="camping8" name="camping8" value="8" type="checkbox"><label for="camping8">Camping 8</label>
-                            </p>
-                            <p>
-                                <input id="camping9" name="camping9" value="9" type="checkbox"><label for="camping9">Camping 9</label>
-                            </p>
-                            <p>
-                                <input id="camping10" name="camping10" value="10" type="checkbox"><label for="camping10">Camping 10</label>
+                            <div class="selectContainer clear">
+                                <select multiple="multiple" name="selectCamping" onchange="">
+                                    <option id="camping1" name="camping1" value="1" type="checkbox">Camping 1</option>
+                                    <option id="camping2" name="camping2" value="2" type="checkbox">Camping 2</option>
+                                    <option id="camping3" name="camping3" value="3" type="checkbox">Camping 3</option>
+                                    <option id="camping4" name="camping4" value="4" type="checkbox">Camping 4</option>
+                                    <option id="camping5" name="camping5" value="5" type="checkbox">Camping 5</option>
+                                    <option id="camping6" name="camping6" value="6" type="checkbox">Camping 6</option>
+                                    <option id="camping7" name="camping7" value="7" type="checkbox">Camping 7</option>
+                                    <option id="camping8" name="camping8" value="8" type="checkbox">Camping 8</option>
+                                    <option id="camping9" name="camping9" value="9" type="checkbox">Camping 9</option>
+                                    <option id="camping10" name="camping10" value="10" type="checkbox">Camping 10</option>
+                                </select>
+                                <ul id="selectCamping">
+                                    <li>Camping 1</li>
+                                    <li>Camping 2</li>
+                                    <li>Camping 3</li>
+                                    <li>Camping 4</li>
+                                    <li>Camping 5</li>
+                                    <li>Camping 6</li>
+                                    <li>Camping 7</li>
+                                    <li>Camping 8</li>
+                                    <li>Camping 9</li>
+                                    <li>Camping 10</li>
+                                </ul>
+                            </div>
+
                             </p>
                         </li>
                         <li>
@@ -100,17 +98,17 @@ include('includes/ce-top.php'); ?>
                             <label>Au</label><input class="date" type="text" name="datepicker-principal-departure" id="datepicker-principal-departure" />
                             <button type=submit>Rechercher</button>
                         </li>
-
                     </ol>
                 </fieldset>
+            </form>
         </div>
-        <div id="datepickerPrincipal" class="datepicker clear"></div>
-        <div id="searchSecondary" class="searchBox">
-            <label>Du</label><input class="date" type="text" name="datepicker-secondary-arrival" id="datepicker-secondary-arrival" />
-            <label>Au</label><input class="date" type="text" name="datepicker-secondary-departure" id="datepicker-secondary-departure" />
-        </div>
-        <div id="datepickerSecondary" class="datepicker clear"></div>
-    </div>
+<!--        <div id="datepickerPrincipal" class="datepicker clear"></div>-->
+<!--        <div id="searchSecondary" class="searchBox">-->
+<!--            <label>Du</label><input class="date" type="text" name="datepicker-secondary-arrival" id="datepicker-secondary-arrival" />-->
+<!--            <label>Au</label><input class="date" type="text" name="datepicker-secondary-departure" id="datepicker-secondary-departure" />-->
+<!--        </div>-->
+<!--        <div id="datepickerSecondary" class="datepicker clear"></div>-->
+
     <!-- colonne pleine largeur -->
 
     <!-- colonne pleine largeur -->
