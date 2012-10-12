3a95ec6d148df3415443e48259d754543085f323
Morgan Brunot <brunot.morgan@gmail.com>
2012-10-12 17:51:47 +0200

    mise en place du formulaire de recherche + appel au modèle resalys * reste à modifier le comportement du ModelType afin de pouvoir changer l'identifiant (de Id passer à Code)

---
+++ src/VacancesDirectesCe/Form/Data/AchatLineaireData.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace VacancesDirectesCe\Form\Data;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\ExecutionContext;
+
+class AchatLineaireData
+{
+    public $nbAdultes = 1;
+    public $pays;
+    public $region;
+    public $campings;
+    public $dateDebut;
+    public $dateFin;
+
+    public function isValide(ExecutionContext $context)
+    {
+        if (!$this->nbAdultes)
+        {
+            $context->addViolation("Le nombre d'adultes doit être renseigné.");
+        }
+
+        if (!$this->dateDebut)
+        {
+            $context->addViolation("La date de début doit être renseignée.");
+        }
+        else
+        {
+            $aDateDebut = explode('/', $this->dateDebut);
+            $dateDebutDate    = new \DateTime(sprintf('%s/%s/%s', $aDateDebut[1], $aDateDebut[0], $aDateDebut[2]));
+            $debutJuilletDate = new \DateTime('07/01/2012');
+
+            $dateDebutTimestamp = $dateDebutDate->getTimestamp();
+
+            if ($dateDebutTimestamp > $debutJuilletDate->getTimestamp())
+            {
+                $context->addViolation("La date de début doit être inférieur au 2 juillet 2012.");
+            }
+        }
+
+        if (!$this->dateFin)
+        {
+            $context->addViolation("La date de fin doit être renseignée.");
+        }
+        else
+        {
+            $aDateFin = explode('/', $this->dateFin);
+            $dateFinDate    = new \DateTime(sprintf('%s/%s/%s', $aDateFin[1], $aDateFin[0], $aDateFin[2]));
+            $finAoutDate = new \DateTime('08/31/2012');
+
+            $dateFinTimestamp = $dateFinDate->getTimestamp();
+
+            if ($dateFinTimestamp < $finAoutDate->getTimestamp())
+            {
+                $context->addViolation("La date de fin doit être supérieur au 30 aout 2012.");
+            }
+        }
+    }
+}
