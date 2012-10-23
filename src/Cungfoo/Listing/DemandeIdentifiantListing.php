3b50794e8d474d02011bed21bf4f2392d926e6b0
Florent Gallardo <f.gallardo@c2is.fr>
2012-10-23 14:25:33 +0200

    Mise en place du formulaire de demande d'identifiant (schema + CRUD + controller + vue) + install d'un vendor pour envoi de mail

---
+++ src/Cungfoo/Listing/DemandeIdentifiantListing.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Cungfoo\Listing;
+
+use Cungfoo\Lib\Listing\Listing,
+    Cungfoo\Lib\Listing\Column;
+
+use Cungfoo\Listing\Base\BaseDemandeIdentifiantListing;
+
+/**
+ * Listing class for 'demande_identifiant' table.
+ *
+ * @author  Morgan Brunot <brunot.morgan@gmail.com>
+ *          Denis Roussel <denis.roussel@gmail.com>
+ * @package propel.generator.Cungfoo.Listing
+ */
+class DemandeIdentifiantListing extends BaseDemandeIdentifiantListing
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configure()
+    {
+        parent::configure();
+    }
+
+} // DemandeIdentifiantListing
