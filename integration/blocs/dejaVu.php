b8936d7a8830fc9887ac890f8129425d08a3db13
LGU <l.guitton@c2is.fr>
2012-10-26 16:59:24 +0200

    bloc deja-vu

--- integration/blocs/dejaVu.php
+++ integration/blocs/dejaVu.php
@@ -1,1 +1,22 @@
-<p>{{ Bloc "Vous avez déjà consulté" }}</p>
+    <div class="element clear">
+        <div class="left">
+            <img class="boxborder" src="http://pimg.devlint.fr/119x86/ccc/333/*" width="119" height="86" alt="" />
+        </div>
+        <div class="details left">
+            <span class="title">Nom camping ***</span>
+            <span class="region">Région</span>
+            <span class="condition">7 nuits pour 6 personnes</span>
+            <a href="#_" class="stain fushia cover"><span>à partir de<br /><span class="price">152<span class="fine">&nbsp;</span>€</span></span></a>
+        </div>
+    </div>
+    <div class="element clear">
+        <div class="left">
+            <img class="boxborder" src="http://pimg.devlint.fr/119x86/ccc/333/*" width="119" height="86" alt="" />
+        </div>
+        <div class="details left">
+            <span class="title">Nom camping ***</span>
+            <span class="region">Région</span>
+            <span class="condition">7 nuits pour 6 personnes</span>
+            <a href="#_" class="stain fushia cover"><span>à partir de<br /><span class="price">152<span class="fine">&nbsp;</span>€</span></span></a>
+        </div>
+    </div>
