043517e35ab02a7263e39148ca67df014793f4f6
Morgan Brunot <brunot.morgan@gmail.com>
2012-11-06 17:33:28 +0100

    lineaire classique coché par défaut

--- src/VacancesDirectesCe/Form/Type/AchatLineaireType.php
+++ src/VacancesDirectesCe/Form/Type/AchatLineaireType.php
@@ -114,7 +114,7 @@ class AchatLineaireType extends AppAwareType
         ));
 
         $builder->add('isBasseSaison', 'choice', array(
-            'choices'  => array('linéaire classique', 'linéaire basse saison'),
+            'choices'  => array('Linéaire classique', 'Linéaire basse saison'),
             'required' => false,
             'expanded' => true,
             'multiple' => false
