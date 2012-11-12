eb5b816c0dcff17fef26dac5653216861f77f858
LGU <l.guitton@c2is.fr>
2012-11-12 18:29:49 +0100

    merge repair

--- web/js/vacancesdirectes/front.js
+++ web/js/vacancesdirectes/front.js
@@ -724,7 +724,9 @@ $(function() {
         $('#searchBlocDate').find('select').sSelect({ddMaxHeight: '300px'});
     }
 
-    initCritResult();
+    if ($('#results').length ){
+        initCritResult();
+    }
 });
 
 /*-- HEADREADY --*/
@@ -1188,4 +1190,227 @@ function initializeAllGmap() {
 
     proxInit();
     infoInit();
+}
+
+
+/*** FONCTIONS RESULTATS DE RECHERCHE ***/
+function initCritResult(){
+
+//variables globales
+    if ( $('.formSearchRefined').length ) {
+        var nbCritChecked = $('#formSearchRefined input:checked').length;
+        $('#nbCrit').text(nbCritChecked);
+    }
+    if ( $('#results').length ) {
+        launchFilters();
+    }
+
+    $('.formSearchRefined .searchGlobalSubmit').click(function(e) {
+        critSelection();
+        displayResults();
+        e.preventDefault();
+    });
+
+    $('#formSearchRefined input:checkbox').change(function() {
+        var nbCritChecked = $('#formSearchRefined input:checked').length;
+        $('#nbCrit').text(nbCritChecked);
+    });
+}
+
+//launcher des filtres
+function launchFilters() {
+
+    console.log('/--- launchFilters ---/');
+
+    items.attr('data-filtered', false);
+
+    //attribution min/max pour le range slider
+    findMinMaxRange();
+
+    //creation du rangeSlider de prix
+    rangeSliderPrice();
+
+    //selection des criteres
+    if ( containerCrit.find('input:checked').length ) {
+        critSelection();
+    }
+
+}
+
+//attribution min/max prix pour le range slider
+function findMinMaxRange() {
+    var allPrices = [];
+    items.each(function() {
+        var itemPrice = parseInt($(this).find('.itemResultBottom :checked + label .price').text());
+        consoleLog(itemPrice);
+        allPrices.push(itemPrice);
+        $(this).attr('data-ranged', true);
+    });
+
+    //prix min
+    Array.min = function(array) {
+        return Math.min.apply(Math, array);
+    };
+    minPrice = Array.min(allPrices);
+
+    //prix max
+    Array.max = function(array) {
+        return Math.max.apply(Math, array);
+    };
+    maxPrice = Array.max(allPrices);
+
+    //items dans la range de prix
+    items = list.find('[data-ranged="true"]');
+
+    console.log('/--- findMinMaxRange ---/ minPrice = '+minPrice+' - maxPrice = '+maxPrice);
+}
+
+//creation du rangeSlider de prix
+function rangeSliderPrice() {
+    var $range = $("#noUiSlider"),
+        minScale = minPrice,
+        maxScale = maxPrice,
+        minStart = minPrice,
+        minStop = maxPrice;
+
+    var initRange = function(){
+        $range.empty().noUiSlider('init', {
+            'scale'		:		[minScale,maxScale],
+            'start'		:		[minStart,minStop],
+
+            change: function(){
+                var values = $(this).noUiSlider('value');
+                $(this).find('.noUi-lowerHandle .rangeBox').text(values[0] + "€");
+                $(this).find('.noUi-upperHandle .rangeBox').text(values[1] + "€");
+            },
+            end: function(type){
+                var values = $(this).noUiSlider('value');
+                valueMin = values[0];
+                valueMax = values[1];
+
+                items.each(function() {
+                    var originPrice = parseInt($(this).find('.itemResultBottom :checked + label .price').text());
+
+                    if ( parseInt(originPrice) >= parseInt(valueMin) && parseInt(originPrice) <= parseInt(valueMax) ) {
+                        $(this).attr('data-ranged', true);
+                    }else{
+                        $(this).attr('data-ranged', false);
+                    }
+                });
+                critSelection();
+                displayResults();
+                console.log('/--- rangeSliderPrice (event: change) ---/');
+            }
+        }).find('.noUi-handle div').each(function(index){
+                $(this).append('<span class="rangeBox">'+$(this).parent().parent().noUiSlider( 'value' )[index]+'</span>');
+            });
+    };
+
+    initRange.call();
+}
+
+//selection des criteres
+function critSelection() {
+
+    //init des tableaux
+    var arrayCrit = [];         //tableau des criteres
+    var arrayCritPlus = [];     //tableau des criteres cumulatifs
+
+    //remplissage de arrayCrit
+    $(containerCrit.find('.contentCrit input:checked')).each(function() {
+        arrayCrit.push($(this).attr('id'));
+    });
+
+    //remplissage de arrayCritPlus (cumulatifs)
+    $(containerCrit.find('.contentCritPlus input:checked')).each(function() {
+        arrayCritPlus.push($(this).attr('id'));
+    });
+    consoleLog(arrayCrit);
+    consoleLog(arrayCritPlus);
+
+    //si aucun critere n'est selectionne
+    if ( arrayCrit == 0 && arrayCritPlus == 0 ) {
+        items.attr('data-filtered', true);
+        console.log('aucun critere selectionne');
+    }else{
+        if ( arrayCrit == 0) {
+            console.log('aucun critere selectionner dans arrayCrit');
+        }else{
+            //boucle de comparaison pour arrayCrit
+            items.each(function() {
+                console.log('-------------------- UN ITEM (crit) ----------------------');
+
+                var itemToShow = $(this);
+
+                //creation des tableau a comparer (avec les valeurs separer par des virgules)
+                var dataCompareCrit = $(this).attr('data-crit').split(',');
+
+                //comparaison des criteres
+                jQuery.each(arrayCrit, function(i) {
+                    if ( jQuery.inArray(arrayCrit[i], dataCompareCrit) > -1 ) {
+                        console.log('critere present : '+this);
+                        itemToShow.attr('data-filtered', true);
+                        return false; //on sort de la boucle car au moins 1 critere est present
+                    }else{
+                        itemToShow.attr('data-filtered', false);
+                    }
+                });
+
+            });
+        }
+
+        if ( arrayCritPlus == 0 ) {
+            console.log('aucun critere selectionner dans arrayCritPlus');
+        }else{
+            //boucle de comparaison pour arrayCritPlus
+
+            /*  if ( arrayCritPlus == 0 ) {
+             itemsFiltered = items;
+             }*/
+            itemsFiltered.each(function() {
+                console.log('-------------------- UN ITEM (critPlus) ----------------------');
+
+                var itemToShow = $(this);
+
+                //creation des tableau a comparer (avec les valeurs separer par des virgules)
+                var dataCompareCritPlus = $(this).attr('data-critPlus').split(',');
+
+                //comparaison des criteresPlus
+                jQuery.each(arrayCritPlus, function(i) {
+                    if ( jQuery.inArray(arrayCritPlus[i], dataCompareCritPlus) > -1 ) {
+                        console.log('critere present : '+this);
+                        itemToShow.attr('data-filtered', true);
+                    }else{
+                        itemToShow.attr('data-filtered', false);
+                        console.log('criterePlus non present : '+this);
+                        return false; //on sort de la boucle car au moins 1 critere n'est pas present
+                    }
+                });
+            });
+        }
+    }
+
+    console.log('/--- critSelection ---/');
+}
+
+//gestion de l'affichage en fonction des criteres + rangeSlider
+function displayResults() {
+    var nbItemsDisplayed = 0;
+
+    items.each(function() {
+        var dataRanged = $(this).attr('data-ranged');
+        var dataFiltered = $(this).attr('data-filtered');
+
+        if ( dataFiltered == 'true' && dataRanged == 'true' ) {
+            $(this).fadeIn().next('.disclaim').fadeIn();
+            nbItemsDisplayed++;
+        }else{
+            $(this).fadeOut().next('.disclaim').fadeOut();
+        };
+    });
+
+    //mise a jour du nombre d'items affiches
+    $('#nbListResultats').text(nbItemsDisplayed);
+
+    console.log('/--- displayResults : '+nbItemsDisplayed+' ---/');
 }
