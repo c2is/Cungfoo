da19fa62a63a4c5f5d9e56ed7c82cabc85aa42e0
Nicolas CHAMALET <n.chamalet@c2is.fr>
2012-11-22 18:34:38 +0100

    [booking] prepartion de la structure

---
+++ integration/includes/booking-top.php
@@ -0,0 +1,21 @@
+<header class="clear">
+    <nav id="nav"><ul class="topnav fixed-width clear">
+        <li class="tab"><a id="home" href="#">
+            <img  src="../web/images/vacancesdirectes/common/pict/home.png" alt="Accueil">
+            <span>Vacances directes.com</span></a>
+        </li>
+    </ul></nav>
+
+    <div id="header" class="fixed-width clear">
+        <div id="headerRight">
+            <a id="account" href="#">Mon compte</a>
+            <div id="telephone">
+                <span>Informations & réservations
+                    <a href="tel:+33825133400">0825 133 400 (0,15€/min)</a>
+                </span>
+            </div>
+        </div>
+        <a href="01_home.php" title="Retourner à Vacances directes | Le mobil-home et vous" id="logo">
+            <img src="../web/images/vacancesdirectes/common/pict/vacancesdirectes-logo.png" alt="Vacances directes | Le mobil-home et vous" /></a>
+    </div>
+</header>
