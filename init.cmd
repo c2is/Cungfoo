af82a8b48e4b6fc853be98e6e71b16c155be4ee4
Nicolas CHAMALET <n.chamalet@c2is.fr>
2012-11-27 18:04:34 +0100

    script de Morgan pour automatiser l'init du projet

--- init.cmd
+++ init.cmd
@@ -1,3 +1,5 @@
+./console cache:clear
 ./vendor/bin/propel-gen app/config/Propel main
 mysql -uroot cungfoo < app/resources/data/sql/Cungfoo.Model.schema.sql
 ./console fixtures:load
+./console cache:clear
