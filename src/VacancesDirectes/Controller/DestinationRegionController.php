ac3a5fcb6185d7d16f7600b688bc613395290f48
Guillaume Manen <g.manen@c2is.fr>
2012-11-28 14:41:57 +0100

    Recuperation des etablissement pour une region dans la page destination region

--- src/VacancesDirectes/Controller/DestinationRegionController.php
+++ src/VacancesDirectes/Controller/DestinationRegionController.php
@@ -4,7 +4,8 @@ namespace VacancesDirectes\Controller;
 
 use Cungfoo\Model\RegionQuery,
     Cungfoo\Model\PointInteretPeer,
-    Cungfoo\Model\EventPeer;
+    Cungfoo\Model\EventPeer,
+    Cungfoo\Model\EtablissementPeer;
 
 use Silex\Application,
     Silex\ControllerCollection,
@@ -31,12 +32,14 @@ class DestinationRegionController implements ControllerProviderInterface
 
             $sitesAVisiter = PointInteretPeer::getForRegion($region, PointInteretPeer::RANDOM_SORT, 5);
             $events        = EventPeer::getForRegion($region, EventPeer::RANDOM_SORT, 5);
+            $campings      = EtablissementPeer::getForRegion($region, EtablissementPeer::RANDOM_SORT, 5);
 
             return $app['twig']->render('Destination/region.twig', array(
                 'locale'        => $locale,
-                'region'          => $region,
+                'region'        => $region,
                 'sitesAVisiter' => $sitesAVisiter,
                 'events'        => $events,
+                'campings'      => $campings,
             ));
         })
         ->bind('destination_region');
