f9516c021ea1a84fb3d7c3a4f260fe385dd6e605
Nicolas CHAMALET <n.chamalet@c2is.fr>
2012-12-04 09:25:09 +0100

    git ignore

--- init.cmd
+++ init.cmd
@@ -3,3 +3,4 @@
 mysql -uroot cungfoo < app/resources/data/sql/Cungfoo.Model.schema.sql
 ./console fixtures:load
 ./console cache:clear
+git checkout src/Cungfoo/Model src/Cungfoo/Listing src/Cungfoo/Form
