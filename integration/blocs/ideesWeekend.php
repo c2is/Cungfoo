ca0fb606aba2bda7f0c80a29d900ff3f9e02f6ca
Nicolas CHAMALET <n.chamalet@c2is.fr>
2012-12-10 15:34:07 +0100

    [all] optmisations CSS/HTML

--- integration/blocs/ideesWeekend.php
+++ integration/blocs/ideesWeekend.php
@@ -2,28 +2,29 @@
     <div class="stamp fushia">
         <h2>Idées week-end</h2>
     </div>
-    <div class="element clear">
+    <div class="element clear linkParent">
         <div class="left">
-            <a href="#_" class="linkBlock"><img class="view" src="http://pimg.devlint.fr/96x86/e6e5e5/333/*" width="96" height="86" alt="" /></a>
+            <img class="view" src="http://pimg.devlint.fr/96x86/e6e5e5/333/*" width="96" height="86" alt="" />
         </div>
-        <div class="details left linkParent">
+        <div class="details left">
             <span class="title">Spécial pont de l'ascension</span>
         </div>
         <a href="#_" class="stain cover linkBlock"><span>à partir de<br /><span class="price linkBlock">152<span class="fine">&nbsp;</span>€</span></span></a>
     </div>
-    <div class="element noprice clear">
+    <div class="element noprice linkParent clear">
         <div class="left">
-            <a href="#_" class="linkBlock"><img class="view" src="http://pimg.devlint.fr/96x86/e6e5e5/333/*" width="96" height="86" alt="" /></a>
+            <img class="view" src="http://pimg.devlint.fr/96x86/e6e5e5/333/*" width="96" height="86" alt="" />
         </div>
-        <div class="details left linkParent">
+        <div class="details left">
             <span class="title">En juin, une nuit offerte</span>
         </div>
+        <a href="#_" class="linkBlock"></a>
     </div>
-    <div class="element clear">
+    <div class="element clear linkParent">
         <div class="left">
-            <a href="#_" class="linkBlock"><img class="view" src="http://pimg.devlint.fr/96x86/e6e5e5/333/*" width="96" height="86" alt="" /></a>
+            <img class="view" src="http://pimg.devlint.fr/96x86/e6e5e5/333/*" width="96" height="86" alt="" />
         </div>
-        <div class="details left linkParent">
+        <div class="details left">
             <span class="title">Famille nombreuse idées week-end</span>
         </div>
         <a href="#_" class="stain cover linkBlock"><span>à partir de<br /><span class="price linkBlock">324<span class="fine">&nbsp;</span>€</span></span></a>
