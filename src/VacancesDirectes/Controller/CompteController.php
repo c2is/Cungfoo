440f24085b5704bf4a864c2b88c340c2d6d2fdeb
Morgan Brunot <brunot.morgan@gmail.com>
2012-12-10 12:11:20 +0100

    Création du controller vide de mon compte

---
+++ src/VacancesDirectes/Controller/CompteController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace VacancesDirectes\Controller;
+
+use Silex\Application,
+    Silex\ControllerCollection,
+    Silex\ControllerProviderInterface;
+
+use Symfony\Component\HttpFoundation\Request,
+    Symfony\Component\HttpKernel\Exception\NotFoundHttpException,
+    Symfony\Component\HttpFoundation\Response,
+    Symfony\Component\Routing\Route;
+
+class CompteController implements ControllerProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function connect(Application $app)
+    {
+        $controllers = $app['controllers_factory'];
+
+        $controllers->match('/', function () use ($app)
+        {
+            return $app->renderView('Compte/index.twig');
+
+        })->bind('compte_index');
+
+        return $controllers;
+    }
+}
