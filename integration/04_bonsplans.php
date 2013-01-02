55aed16375abaa85078613b65655cc75ec6ee2cc
Nicolas CHAMALET <n.chamalet@c2is.fr>
2013-01-02 17:53:03 +0100

    [bons plans] HTML/CSS: finalisation de la page

--- integration/04_bonsplans.php
+++ integration/04_bonsplans.php
@@ -6,87 +6,108 @@
 
     <div id="wrap" class="fixed-width clear">
     <!-- colonne gauche -->
-        <div class="column left">
-            <nav id="ficheShare">
-                <a href="#_" class="icon facebook" title="Partager sur Facebook">Partager sur Facebook</a>
-                <a href="#_" class="icon twitter" title="Partager sur Twitter">Partager sur Twitter</a>
-                <a href="#_" class="icon email" title="Partager par email">Partager par email</a>
-                <a href="#_" class="icon print" title="Imprimer">Imprimer</a>
-            </nav>
+        <div class="clear">
+            <div class="column left">
+                <nav id="ficheShare">
+                    <a href="#_" class="icon facebook" title="Partager sur Facebook">Partager sur Facebook</a>
+                    <a href="#_" class="icon twitter" title="Partager sur Twitter">Partager sur Twitter</a>
+                    <a href="#_" class="icon email" title="Partager par email">Partager par email</a>
+                    <a href="#_" class="icon print" title="Imprimer">Imprimer</a>
+                </nav>
 
-            <a href="#semainier" class="bt gris left goto">&lt; Retour</a>
+                <a href="#semainier" class="bt gris left goto">&lt; Retour</a>
 
-            <ol class="pathway" itemscope itemtype="http://data-vocabulary.org/Breadcrumb">
-                <li>&gt; <a href="#_" itemprop="url"><span itemprop="title">Accueil</span></a></li>
-                <li>&gt; <a href="#_" itemprop="url"><span itemprop="title">Nos campings : Campings France</span></a></li>
-                <li>&gt; <a href="#_" itemprop="url"><span itemprop="title">Location mobil home Languedoc-Roussillon</span></a></li>
-                <li>&gt; <a href="#_" itemprop="url"><span itemprop="title">Le Petit Mousse</span></a></li>
-            </ol>
+                <ol class="pathway" itemscope itemtype="http://data-vocabulary.org/Breadcrumb">
+                    <li>&gt; <a href="#_" itemprop="url"><span itemprop="title">Accueil</span></a></li>
+                    <li>&gt; <a href="#_" itemprop="url"><span itemprop="title">Nos campings : Campings France</span></a></li>
+                    <li>&gt; <a href="#_" itemprop="url"><span itemprop="title">Location mobil home Languedoc-Roussillon</span></a></li>
+                    <li>&gt; <a href="#_" itemprop="url"><span itemprop="title">Le Petit Mousse</span></a></li>
+                </ol>
 
-            <h1>Faites le plein de bons plans</h1>
-            <h2 class="h3-like">Prêt à vous faire plaisir à des prix mini ? A vos marques... partez !</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tempus lorem id odio vulputate sed malesuada diam pretium. In pharetra bibendum velit, in consequat odio ullamcorper molestie. Vivamus semper massa metus, eu malesuada lacus. Nunc eu arcu ipsum. Nunc cursus tortor a odio cursus malesuada. Nunc dui nunc, feugiat vitae luctus sed, vehicula nec nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae nunc libero, non vehicula metus. Vestibulum est risus, placerat vel vestibulum ac, euismod eget lacus. Vestibulum ante sapien, lobortis id vehicula non, accumsan sit amet augue. </p>
-            <p>Suspendisse tellus eros, rutrum vel interdum quis, fermentum quis turpis. Aenean velit arcu, facilisis lacinia ullamcorper non, semper sit amet dolor. Sed nisl orci, malesuada nec hendrerit quis, rutrum non ligula. Donec eget velit dui, at condimentum sem. Phasellus vehicula fermentum consectetur. Maecenas sit amet commodo arcu. Aliquam erat volutpat. Quisque blandit, enim at gravida scelerisque, massa risus interdum mauris, sed volutpat metus dolor quis neque.</p>
+                <h1>Faites le plein de bons plans</h1>
+                <h2 class="h3-like">Prêt à vous faire plaisir à des prix mini ? A vos marques... partez !</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tempus lorem id odio vulputate sed malesuada diam pretium. In pharetra bibendum velit, in consequat odio ullamcorper molestie. Vivamus semper massa metus, eu malesuada lacus. Nunc eu arcu ipsum. Nunc cursus tortor a odio cursus malesuada. Nunc dui nunc, feugiat vitae luctus sed, vehicula nec nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae nunc libero, non vehicula metus. Vestibulum est risus, placerat vel vestibulum ac, euismod eget lacus. Vestibulum ante sapien, lobortis id vehicula non, accumsan sit amet augue. </p>
+                <p>Suspendisse tellus eros, rutrum vel interdum quis, fermentum quis turpis. Aenean velit arcu, facilisis lacinia ullamcorper non, semper sit amet dolor. Sed nisl orci, malesuada nec hendrerit quis, rutrum non ligula. Donec eget velit dui, at condimentum sem. Phasellus vehicula fermentum consectetur. Maecenas sit amet commodo arcu. Aliquam erat volutpat. Quisque blandit, enim at gravida scelerisque, massa risus interdum mauris, sed volutpat metus dolor quis neque.</p>
+            </div>
+            <!-- // colonne gauche -->
+            <!-- colonne droite -->
+            <aside class="column right">
+                <?php include('blocs/search.php') ?>
+            </aside>
+            <!-- // colonne droite -->
         </div>
-    <!-- // colonne gauche -->
-    <!-- colonne droite -->
-        <aside class="column right">
-            <?php include('blocs/search.php') ?>
-        </aside>
-    <!-- // colonne droite -->
+
     <!-- colonne full -->
-        <div class="column left">
+        <div class="column clearboth clear">
             <h2 class="h3-like">Nos offres</h2>
-            <div id="bpItemsList">
+            <div id="bpItemsList" class="clear">
                 <div class="bpItem itemResult bgImg">
                     <div class="itemResultTitle">Vos vacances les pieds dans l'eau</div>
                     <div class="view">
                         <img class="bg" src="http://pimg.devlint.fr/608x155/ccc/333/*" alt="" />
                     </div>
                     <div class="content clear">
-                        A partir de 952€
                         <p>Suspendisse tellus eros, rutrum vel interdum quis, fermentum quis turpis. Aenean velit arcu, facilisis lacinia ullamcorper non, semper sit amet dolor. Sed nisl orci, malesuada nec hendrerit quis, rutrum non ligula.</p>
                         <div class="bpCount left">
-                            <span>Plus que</span>
-                            <span class="bpDays"></span>
-                            <span class="bpHours"></span>
-                            <span class="bpMinutes"></span>
+                            Plus que<br />
+                            <span>23J</span>
+                            <span>12H</span>
+                            <span>22MN</span>
                         </div>
-                        <a href="#" class="bt sombre big ib right popinBP">Voir l'offre</a>
+                        <div class="stain fushia cover linkBlock" href="#" style="background-size: cover;">
+                        <span>
+                            à partir de<br>
+                            <span class="price linkBlock">244<span class="fine">&nbsp;</span>€</span>
+                            la semaine
+                        </span>
+                        </div>
+                        <a href="#" class="bt fushia big ib right popinBP">Voir l'offre</a>
                     </div>
                 </div>
                 <div class="bpItem itemResult bgImg half">
                     <div class="itemResultTitle">Vos vacances les pieds dans l'eau</div>
                     <div class="view">
-                        <img class="bg" src="http://pimg.devlint.fr/608x155/ccc/333/*" alt="" />
+                        <img class="bg" src="http://pimg.devlint.fr/295x155/ccc/333/*" alt="" />
                     </div>
                     <div class="content clear">
-                        A partir de 952€
                         <p>Suspendisse tellus eros, rutrum vel interdum quis, fermentum quis turpis. Aenean velit arcu, facilisis lacinia ullamcorper non, semper sit amet dolor. Sed nisl orci, malesuada nec hendrerit quis, rutrum non ligula.</p>
                         <div class="bpCount left">
-                            <span>Plus que</span>
-                            <span class="bpDays"></span>
-                            <span class="bpHours"></span>
-                            <span class="bpMinutes"></span>
+                            Plus que<br />
+                            <span>23J</span>
+                            <span>12H</span>
+                            <span>22MN</span>
                         </div>
-                        <a href="#" class="bt sombre big ib right popinBP">Voir l'offre</a>
+                        <div class="stain fushia cover linkBlock" href="#" style="background-size: cover;">
+                        <span>
+                            à partir de<br>
+                            <span class="price linkBlock">244<span class="fine">&nbsp;</span>€</span>
+                            la semaine
+                        </span>
+                        </div>
+                        <a href="#" class="bt fushia big ib right popinBP">Voir l'offre</a>
                     </div>
                 </div>
                 <div class="bpItem itemResult bgImg half">
                     <div class="itemResultTitle">Vos vacances les pieds dans l'eau</div>
                     <div class="view">
-                        <img class="bg" src="http://pimg.devlint.fr/608x155/ccc/333/*" alt="" />
+                        <img class="bg" src="http://pimg.devlint.fr/295x155/ccc/333/*" alt="" />
                     </div>
                     <div class="content clear">
-                        A partir de 952€
                         <p>Suspendisse tellus eros, rutrum vel interdum quis, fermentum quis turpis. Aenean velit arcu, facilisis lacinia ullamcorper non, semper sit amet dolor. Sed nisl orci, malesuada nec hendrerit quis, rutrum non ligula.</p>
                         <div class="bpCount left">
-                            <span>Plus que</span>
-                            <span class="bpDays"></span>
-                            <span class="bpHours"></span>
-                            <span class="bpMinutes"></span>
+                            Plus que<br />
+                            <span>23J</span>
+                            <span>12H</span>
+                            <span>22MN</span>
                         </div>
-                        <a href="#" class="bt sombre big ib right popinBP">Voir l'offre</a>
+                        <div class="stain fushia cover linkBlock" href="#" style="background-size: cover;">
+                        <span>
+                            à partir de<br>
+                            <span class="price linkBlock">244<span class="fine">&nbsp;</span>€</span>
+                            la semaine
+                        </span>
+                        </div>
+                        <a href="#" class="bt fushia big ib right popinBP">Voir l'offre</a>
                     </div>
                 </div>
             </div>
