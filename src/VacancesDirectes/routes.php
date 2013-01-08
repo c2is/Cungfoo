0b6803c2ee09d70deff3e67886ecf90f5892a774
Florent Gallardo <f.gallardo@c2is.fr>
2013-01-08 15:00:46 +0100

    Mise en place des filtres de recherche

--- src/VacancesDirectes/routes.php
+++ src/VacancesDirectes/routes.php
@@ -42,6 +42,7 @@ $app->mount('/compte',                                new Controller\CompteContr
 $app->mount('/vos-week-ends',                         new Controller\VosWeekEndsController());
 $app->mount('/esi',                                   new Controller\EsiController());
 $app->mount('/assurance',                             new Controller\AnnulationController());
+$app->mount('/search_filter',                         new Controller\SearchFilterController());
 
 $app->match('/top-campings',      'VacancesDirectes\Controller\TopCampingController::indexAction')->bind('top_campings');
 
