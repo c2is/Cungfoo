63ae03421bbc287e45b81a1ac5d9125bdcca9a5e
Morgan Brunot <brunot.morgan@gmail.com>
2013-01-16 15:22:33 +0100

    suppression des tables site+domaine + nettoyage cungfoo

--- src/Cungfoo/Form/Type/ContextType.php
+++ src/Cungfoo/Form/Type/ContextType.php
@@ -14,7 +14,6 @@ class ContextType extends AppAwareType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $this
-            ->addDimensions($builder)
             ->addLocale($builder)
             ->addTerm($builder)
         ;
@@ -26,25 +25,6 @@ class ContextType extends AppAwareType
     public function getName()
     {
         return 'Context';
-    }
-
-    protected function addDimensions($builder)
-    {
-        $dimensions = $this->app['config']->get('dimensions');
-        foreach ($dimensions as $dimensionName => $dimensionInformation)
-        {
-            $dimensionQuery = $dimensionInformation['class'] . 'Query';
-            $builder->add($dimensionName, 'model', array(
-                'class'         => $dimensionInformation['class'],
-                'label'         => sprintf('context.%s', $dimensionName),
-                'required'      => false,
-                'data_class'    => null,
-                'data'          => $dimensionQuery::create()->filterById($this->app['context']->get($dimensionName))->findOne(),
-                'empty_value'   => sprintf('%s.all', $dimensionName)
-            ));
-        }
-
-        return $this;
     }
 
     protected function addLocale($builder)
--- src/VacancesDirectesCe/context.php
+++ src/VacancesDirectesCe/context.php
@@ -11,11 +11,5 @@ $app['context']->addParam('site', isset($_GET['site']) ? $_GET['site'] : 'site.c
 $app['context']->addParam('domaine', isset($_GET['domaine']) ? $_GET['domaine'] : 'fr');
 $app['context']->addParam('language', isset($_GET['domaine']) ? $_GET['domaine'] : $app['context']->get('domaine'));
 
-// check if domain is allow for this site
-if (!in_array($app['context']->get('domaine'), $app['config']->get('dimensions')['site'][$app['context']->get('site')]['domaines']))
-{
-    throw new \Exception(sprintf("`%s` domain is not available for `%s` site.", $app['context']->get('domaine'), $app['context']->get('site')));
-}
-
 // set current language
 $app['translator']->setLocale($app['context']->get('language'));
