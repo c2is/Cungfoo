df30f21e217889581ab444f856a80a677be45c64
Morgan Brunot <brunot.morgan@gmail.com>
2013-01-17 10:10:26 +0100

    Gestion des droits utllisateur

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -15,10 +15,11 @@ $app['config'] = $app->share(function() {
 });
 
 $app['config']->addParams(array(
-    'web_dir'       => sprintf('%s/web', $app['config']->get('root_dir')),
-    'languages'     => Symfony\Component\Yaml\Yaml::parse(sprintf('%s/languages.yml', $app['config']->get('config_dir')))['languages'],
-    'dimensions'    => Symfony\Component\Yaml\Yaml::parse(sprintf('%s/dimensions.yml', $app['config']->get('config_dir')))['dimensions'],
-    'version'       => trim(file_get_contents(sprintf('%s/version', $app['config']->get('config_dir')))),
+    'web_dir'    => sprintf('%s/web', $app['config']->get('root_dir')),
+    'languages'  => Symfony\Component\Yaml\Yaml::parse(sprintf('%s/languages.yml', $app['config']->get('config_dir')))['languages'],
+    'dimensions' => Symfony\Component\Yaml\Yaml::parse(sprintf('%s/dimensions.yml', $app['config']->get('config_dir')))['dimensions'],
+    'version'    => trim(file_get_contents(sprintf('%s/version', $app['config']->get('config_dir')))),
+    'security'   => new Cungfoo\Lib\Crud\Security($app),
 ));
 
 /* T W I G  C O N F I G U R A T I O N  */
--- src/Cungfoo/security.php
+++ src/Cungfoo/security.php
@@ -8,6 +8,8 @@
  * @package Cungfoo by C2IS
  */
 
+use Cungfoo\Lib\Crud\Security as CrudSecurity;
+
 /* S E C U R I T Y   M A N A G E R */
 $app->register(new Silex\Provider\SecurityServiceProvider(), array(
     'security.firewalls' => array(
@@ -20,5 +22,14 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
             'logout'    => array('logout_path' => '/logout'),
             'users'     => Symfony\Component\Yaml\Yaml::parse(sprintf('%s/Cungfoo/users.yml', $app['config']->get('config_dir')))['users'],
         )
-    )
+    ),
 ));
+
+$app['config']->security->load(sprintf('%s/Cungfoo/security.yml', $app['config']->get('config_dir')));
+
+$app->before(function() use ($app) {
+    if (!$app['config']->security->isGranted())
+    {
+        $app->abort(403, "No permission to access.");
+    }
+});
