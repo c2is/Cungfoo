6f77881a54b30f2349019eb83a65941c552f3d01
Morgan Brunot <brunot.morgan@gmail.com>
2013-01-23 10:54:38 +0100

    selection automatique de la langue courante

--- web/js/cungfoo/main.js
+++ web/js/cungfoo/main.js
@@ -3,6 +3,16 @@
  *                                              DOM ready
  * ///////////////////////////////////////////////////////////////////////////////////////////////////////////
  */
+$.fn.datepicker.dates = []
+$.fn.datepicker.dates['fr'] = {
+    days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+    daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
+    months: ["Janvier", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    today: "Today"
+};
+
 $(function() {
 
     $("input[id$='_zip'], input[id$='_zipcode'], input[id$='_capacite'], input[id$='_priority'], input[id$='_age']").addClass('span1');
@@ -18,6 +28,14 @@ $(function() {
         weekStart: 1
     });
 
+
+    $('#crudGroup a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
+
+    $('#crudGroup a:first').tab('show');
+
     if($('#crudGroupContent .alert-error').length > 0){
         var paneId = $('#crudGroupContent .alert-error:first').parents('.tab-pane').attr('id');
         $('#crudGroup a[href="#' + paneId + '"]').tab('show');
@@ -27,6 +45,7 @@ $(function() {
         $('.tabbable .nav-tabs a[href="#' + localizedPaneId + '"]').tab('show');
     }
 
+    $('#transGroup .nav-tabs a[href="#' + locale + '"]').tab('show');
 
     //via http://ivaynberg.github.com/select2/#documentation
     //$("select[multiple='multiple']").select2({
