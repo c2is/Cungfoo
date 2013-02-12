d11f79c63468f3a0a92f9ae74cc80b7d2a8c48c3
Guillaume Manen <guillaume.manen@gmail.com>
2013-02-12 18:04:11 +0100

    Interface d'administration des libelles statiques

--- src/Cungfoo/Controller/I18nController.php
+++ src/Cungfoo/Controller/I18nController.php
@@ -6,12 +6,16 @@ use Silex\Application,
     Silex\ControllerCollection,
     Silex\ControllerProviderInterface;
 
-use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Yaml\Yaml,
+    Symfony\Component\HttpFoundation\Request;
+
 /**
  * 
  */
 class I18nController implements ControllerProviderInterface
 {
+    const LOCALES_PATTERN = '%s/app/config/VacancesDirectes/locales/%s.yml';
+
     /**
      * {@inheritdoc}
      */
@@ -19,24 +23,44 @@ class I18nController implements ControllerProviderInterface
     {
         $controllers = $app['controllers_factory'];
 
-        $controllers->get('/', function () use ($app)
+        $controllers->match('/', function (Request $request) use ($app)
         {
+            if ($request->getMethod() == 'POST')
+            {
+                foreach ($app['config']->get('languages') as $locale => $language)
+                {
+                    file_put_contents(sprintf(self::LOCALES_PATTERN, $app['config']->get('root_dir'), $locale), Yaml::dump($request->request->get($locale)));
+                }
+
+                return $app->redirect($app->path('i18n_admin'));
+            }
+
             $translations = array();
             $keys = array();
+            $tabbedKeys = array();
+            $langues = array();
 
             foreach ($app['config']->get('languages') as $locale => $language)
             {
-                $yaml = sprintf('%s/app/config/VacancesDirectes/locales/%s.yml', $app['config']->get('root_dir'), $locale);
-                $translationsLocale = Yaml::parse($yaml);
-                $translations[] = $translationsLocale;
+                $translationsLocale = Yaml::parse(sprintf(self::LOCALES_PATTERN, $app['config']->get('root_dir'), $locale));
+
+                $translations[$locale] = $translationsLocale;
                 $keys += $translationsLocale;
+
+                $langues[] = $locale;
             }
 
             $keys = array_keys($keys);
-var_dump($keys);die;
+            foreach ($keys as $key)
+            {
+                $explodedKey = explode('.', $key);
+                $tabbedKeys[$explodedKey[0]][] = $key;
+            }
+
             return $app['twig']->render('I18n/list.twig', array(
                 'translations' => $translations,
-                'keys' => $keys,
+                'tabbedKeys' => $tabbedKeys,
+                'langues' => $langues,
             ));
         })
         ->bind('i18n_admin');
