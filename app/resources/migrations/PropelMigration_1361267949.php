f99cd23fa2093b02ac563c3fa2de4d623d75b58b
Morgan Brunot <brunot.morgan@gmail.com>
2013-02-19 11:01:37 +0100

    correction de migration suite à failed de mise en production

---
+++ app/resources/migrations/PropelMigration_1361267949.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * Data object containing the SQL and PHP code to migrate the database
+ * up to version 1361267949.
+ * Generated on 2013-02-19 10:59:09 by m.brunot
+ */
+class PropelMigration_1361267949
+{
+
+    public function preUp($manager)
+    {
+        // add the pre-migration code here
+    }
+
+    public function postUp($manager)
+    {
+        // add the post-migration code here
+    }
+
+    public function preDown($manager)
+    {
+        // add the pre-migration code here
+    }
+
+    public function postDown($manager)
+    {
+        // add the post-migration code here
+    }
+
+    /**
+     * Get the SQL statements for the Up migration
+     *
+     * @return array list of the SQL strings to execute for the Up migration
+     *               the keys being the datasources
+     */
+    public function getUpSQL()
+    {
+        return array (
+  'cungfoo' => '
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+ALTER TABLE `etablissement` DROP FOREIGN KEY `etablissement_FK_3`;
+
+DROP INDEX `etablissement_FI_3` ON `etablissement`;
+
+DROP INDEX `etablissement_FI_4` ON `etablissement`;
+
+CREATE INDEX `etablissement_FI_3` ON `etablissement` (`categorie_id`);
+
+CREATE INDEX `etablissement_FI_4` ON `etablissement` (`related_1`);
+
+CREATE INDEX `etablissement_FI_5` ON `etablissement` (`related_2`);
+
+ALTER TABLE `etablissement` ADD CONSTRAINT `etablissement_FK_3`
+    FOREIGN KEY (`categorie_id`)
+    REFERENCES `categorie` (`id`)
+    ON DELETE SET NULL;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
+',
+);
+    }
+
+    /**
+     * Get the SQL statements for the Down migration
+     *
+     * @return array list of the SQL strings to execute for the Down migration
+     *               the keys being the datasources
+     */
+    public function getDownSQL()
+    {
+        return array (
+  'cungfoo' => '
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+ALTER TABLE `etablissement` DROP FOREIGN KEY `etablissement_FK_3`;
+
+DROP INDEX `etablissement_FI_5` ON `etablissement`;
+
+DROP INDEX `etablissement_FI_3` ON `etablissement`;
+
+DROP INDEX `etablissement_FI_4` ON `etablissement`;
+
+CREATE INDEX `etablissement_FI_3` ON `etablissement` (`related_1`);
+
+CREATE INDEX `etablissement_FI_4` ON `etablissement` (`related_2`);
+
+ALTER TABLE `etablissement` ADD CONSTRAINT `etablissement_FK_3`
+    FOREIGN KEY (`related_1`)
+    REFERENCES `etablissement` (`id`)
+    ON DELETE SET NULL;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
+',
+);
+    }
+
+}
