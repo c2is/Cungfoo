e399c2887c20f0ae54e419ddadfe4c9ff81f46d4
Morgan Brunot <brunot.morgan@gmail.com>
2013-04-04 15:42:04 +0200

    Bon plan NB jour resalys de 1-21

--- src/Cungfoo/Model/map/BonPlanTableMap.php
+++ src/Cungfoo/Model/map/BonPlanTableMap.php
@@ -63,9 +63,27 @@ class BonPlanTableMap extends TableMap
 ));
         $this->addColumn('day_range', 'DayRange', 'ENUM', true, null, null);
         $this->getColumn('day_range', false)->setValueSet(array (
-  0 => '7',
-  1 => '14',
-  2 => '21',
+  0 => '1',
+  1 => '2',
+  2 => '3',
+  3 => '4',
+  4 => '5',
+  5 => '6',
+  6 => '7',
+  7 => '8',
+  8 => '9',
+  9 => '10',
+  10 => '11',
+  11 => '12',
+  12 => '13',
+  13 => '14',
+  14 => '15',
+  15 => '16',
+  16 => '17',
+  17 => '18',
+  18 => '19',
+  19 => '20',
+  20 => '21',
 ));
         $this->addColumn('nb_adultes', 'NbAdultes', 'INTEGER', false, null, 1);
         $this->addColumn('nb_enfants', 'NbEnfants', 'INTEGER', false, null, 0);
