4e4b82e3537da4a368f0ff94803af15b75820957
gservoz <gservoz@village-center.fr>
2013-08-08 14:09:21 +0200

    [#1844] et [#1907] : gestion des retours ogone (couloir).

--- src/VacancesDirectes/Controller/CouloirController.php
+++ src/VacancesDirectes/Controller/CouloirController.php
@@ -80,7 +80,9 @@ class CouloirController implements ControllerProviderInterface
         ->bind('couloir_detail_sejour');
 
         $controllers->match('/' . $app->trans('seo.url.couloir.recapitulatif') . '/{proposalKey}', function (Request $request, $proposalKey) use ($app) {
-
+			
+			$gateway_messages = array('LANG_ogone_canceled', 'LANG_ogone_declined');
+								
             $rslConfig = $this->getResalysConfig($app);
 
             $query = array(
@@ -89,8 +91,6 @@ class CouloirController implements ControllerProviderInterface
                 "webuser"       => $rslConfig['username'],
                 "tokens"        => 'ignore_token',
                 "display"       => 'cart_payment',
-				"ogone_accepted_route" => '/'.$app->trans('seo.url.couloir.index').'/'.$app->trans('seo.url.couloir.confirmation'),
-				"ogone_back_route" => '/'.$app->trans('seo.url.couloir.index').'/'.$app->trans('seo.url.couloir.recapitulatif'),
                 "actions"       => 'updateReservationContent;BupdateReservationAddPrestations;BupdateCartReservations',
                 "proposal_key"  => $proposalKey,
                 "confirmation"  => $app['url_generator']->generate('couloir_confirmation', array(), true),
@@ -98,7 +98,12 @@ class CouloirController implements ControllerProviderInterface
                 "backLink"      => 'history.go(-1);',
                 "cgv"           => $app['url_generator']->generate('edito_by_slug', array('slug' => 'conditions-generales'), true),
                 "session"       => base64_decode(str_replace('B64_', '', $request->cookies->get('session_name'))),
+				"ogone_accepted_route" => '/'.$app->trans('seo.url.couloir.index').'/'.$app->trans('seo.url.couloir.confirmation'),
+				"ogone_back_route" => '/'.$app->trans('seo.url.couloir.index').'/'.$app->trans('seo.url.couloir.recapitulatif'),
             );
+			
+			if(in_array($request->query->get('gateway_message'),$gateway_messages)) 
+				$query['gateway_message'] = $request->query->get('gateway_message');
 
             $query = array_merge($query, $request->request->all());
 
@@ -116,6 +121,8 @@ class CouloirController implements ControllerProviderInterface
         $controllers->get('/' . $app->trans('seo.url.couloir.confirmation') . '', function (Request $request) use ($app) {
 
             $rslConfig = $this->getResalysConfig($app);
+			
+			$gateway_messages = array('LANG_ogone_exception', 'LANG_ogone_accepted');
 
             $query = array(
                 "specificFiles" => 'couloir',
@@ -127,7 +134,10 @@ class CouloirController implements ControllerProviderInterface
                 "session"       => base64_decode(str_replace('B64_', '', $request->cookies->get('session_name'))),
                 "tokens"        => $request->query->get('tokens')
             );
-
+			
+			if(in_array($request->query->get('gateway_message'),$gateway_messages)) 
+				$query['gateway_message'] = $request->query->get('gateway_message');
+			
             $query = array_merge($query, $request->request->all());
 
             $query = $this->pushCookieSession($request, $query);
