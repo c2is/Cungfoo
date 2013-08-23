bedcea0832880ddd48614f20462fd415daf2b325
gservoz <gservoz@village-center.fr>
2013-08-23 14:04:03 +0200

    #2087 implémentation tag ematch + fix erreur Notice

--- clear_cache.sh
+++ clear_cache.sh
@@ -1,27 +1,10 @@
 #!/bin/sh
 #
 # 2013-08-14
-# @GSP: vide le cache des frontaux en prod
-#		ou le front en preprod
+# @GSP: vide le cache
 #
-server_name="$(uname -n)"
-if [ "$server_name" = "web04.vacancesdirectes-prod.resalys.com"\
-  -o "$server_name" = "web05.vacancesdirectes-prod.resalys.com" ]
-then
-	echo 'connecting to web04'
-	ssh web04.vacancesdirectes-prod.resalys.com "
-	  cd /var/www/local/virtualdomains/343/vacances-directes.com/current && rm -rf app/cache/*
-	  php -d memory_limit=256M ./console cache:clear
-	  exit
-	"
-	echo 'connecting to web05'
-	ssh web05.vacancesdirectes-prod.resalys.com "
-	  cd /var/www/local/virtualdomains/343/vacances-directes.com/current && rm -rf app/cache/*
-	  php -d memory_limit=256M ./console cache:clear
-	  exit
-	"
-	echo 'done'
-else
-	rm -rf app/cache/*
-	php -d memory_limit=256M ./console cache:clear
-fi
+
+echo "connected to $(uname -n)"
+cd /var/www/local/virtualdomains/343/vacances-directes.com/current && rm -rf app/cache/*
+#php -d memory_limit=256M ./console cache:clear
+echo 'Cache cleared.'
