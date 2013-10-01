73ae702f128c0f6f3be438829cdc173fa4f3d036
gservoz <gservoz@village-center.fr>
2013-10-01 14:27:31 +0200

    #2147 ajout landing BE

---
+++ web/racine_be/index.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html class="js-false" lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Camping &amp; Location mobil home | Vacances Directes</title>
+
+    <meta name="description" content="" />
+
+    <!-- Google Webmaster Tools .be -->
+    <meta name="google-site-verification" content="L2kSluHq9vOHhhpWPCIdxmqPsL8dy9jccVKQh980_8o" />
+	
+    <link rel="shortcut icon" type="image/x-icon" href="http://cdn.vacances-directes.com/favicon.ico" />
+
+    <link rel="canonical" href="http://www.vacances-directes.be/" />
+	
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(
+            ['_setAccount', 'UA-4231998-1'], // compte FR
+            ['_trackPageview'],
+            ['_gat._anonymizeIp'],
+            ['_setAllowLinker', true],
+            ['b._setAccount', 'UA-17590883-1'], // compte DE
+            ['b._trackPageview'],
+            ['b._gat._anonymizeIp'],
+            ['b._setAllowLinker', true]
+        );
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s  = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+	<style>
+	body {
+		background: url("http://cdn.vacances-directes.com/images/vacancesdirectes/common/pict/bg.jpg") no-repeat scroll 50% 0 #F5F5F5;
+		color: #464545;
+		font: 12px Arial,Helvetica,sans-serif;
+	}
+	#header, #wrap {
+		margin: auto;
+		width: 288px;
+	}
+	#wrap { width : 400px; }
+	h1 {
+		height: 0;
+		margin: 0;
+		padding: 0;
+		text-indent: -9999px;
+	}
+	a, a:active, a:hover, a:visited {
+		color: #444;
+		text-decoration: none;
+		margin: 50px 20px 0 20px;
+		text-align: center;	
+		width: 120px;
+	}
+	#wrap img { box-shadow : 0 0 5px #444; }
+	</style>
+</head>
+<body>
+	<div id="header" class="fixed-width clear">
+		<h1 id="logoTitle">Camping et Location mobil-home France, Espagne, Italie</h1>
+		<img src="http://cdn.vacances-directes.com/images/vacancesdirectes/common/pict/vacancesdirectes-logo_fr.png" width="288px" height="131px" alt="Vacances directes | Le camping tout simplement !" />
+	</div>
+
+    <div id="wrap" class="fixed-width clear">
+		<a style="float:left;" href="http://www.vacances-directes.com" >
+			<img src="/fr_100.jpg" title="Vacances Directes"/>
+			<br><br>Poursuivre sur la version Française
+		</a>
+		<a style="float:right" href="http://www.vacances-directes.nl" >
+			<img src="/nl_100.jpg" title=""/>
+			<br><br>U gaat nu verder op de Nederlandse versie van de website
+		</a>
+	</div>
+
+</body>
+</html>
